6e0016d4006fee2384aa104a25d49ce98870e9bc
Mike Green <greens1593@gmail.com>
2023-01-19 12:07:22 +0200

    Завершена разработка навбара. Навбар работает и для десктопа и для мобильного.

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import './Navbar.scss';
 import { images } from '../../constans';
+import { useState } from 'react';
 
 const navLinks = ['home', 'about', 'work', 'skills', 'contact'];
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -14,12 +21,29 @@ const Navbar = () => {
       <ul className='app__navbar-links'>
           {navLinks.map((item) => (
             <li className='app__flex p-text' key={`link-${item}`}>
-              <div>
-                <a href={`#${item}`}>{item}</a>
-              </div>
+              <div></div>
+              <a href={`#${item}`}>{item}</a>
             </li>
           ))}
       </ul>
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{duration: 0.85, ease: 'easeInOut'}}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {navLinks.map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)} >{item}</a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   )
 }
