de8845bd48de6ed57a6e737773793b10e10ae1a7
Mike Green <greens1593@gmail.com>
2023-01-20 13:07:53 +0200

    Фронтенд связан с бэкендом на санити. В секции about карточки заполняются содержимым базы данных.

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,18 +1,23 @@
 import React, {useEffect, useState} from 'react'
 import {motion} from 'framer-motion'
 
-import {images} from '../../constans'
-
 import './About.scss'
 
-const abouts = [
-  { title: "Web Development", description: 'I am good web deweloper', img: images.about01 },
-  {title: "Web Design", description: 'I am good web designer', img: images.about02},
-  { title: "Frontend", description: 'I am good web frontend deweloper', img: images.about03 },
-  {title: "Backend", description: 'I am good web backend deweloper', img: images.about04}
-]
+import { urlFor, client } from '../../client'
+
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([])
+  useEffect(() => {
+    const query = `*[_type == "abouts"]`
+    client.fetch(query)
+      .then(data => {
+        setAbouts(data)
+      })
+  }, [])
+  
+
   return (
     <>
       <h2 className='head-text'>I know That <span>Good Apps</span> <br /> means <span>Good Bussines</span></h2>
@@ -23,9 +28,9 @@ const About = () => {
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
             className='app__profile-item'
-            key={about.title + about.index}
+            key={about.title + index}
           >
-            <img src={about.img} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
